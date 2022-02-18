67b16754df9d5f0f4d2ece3255f14cc58dedf1d6
Nicolas Fiedler <nicolas.fiedler@dbccompany.com.br>
2022-02-17 22:14:28 -0300

    exe1

---
+++ aula-2/src/com/dbc/Exe1.java
@@ -0,0 +1,23 @@
+package com.dbc;
+
+import java.util.Scanner;
+
+public class Exe1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Double valor, valorUni, total;
+
+        System.out.printf("Nome do produto: ");
+        String produto = scanner.nextLine();
+        System.out.printf("Valor: ");
+        valor = scanner.nextDouble();
+
+        System.out.println("\n\n\n\n\n\n\n\n\n\n " +
+                "Promocao de " + produto + "\n--------------------------");
+        for (double i = 1, j = 0.05; i <= 10; i++, j += 0.05) {
+            valorUni = valor - (valor * j);
+            total = valorUni * i;
+            System.out.printf("%.0f x R$ %.2f = %.2f\n", i, valorUni, total);
+        }
+    }
+}
---
+++ aula-2/src/com/dbc/Main.java
@@ -0,0 +1,56 @@
+package com.dbc;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        int vet[] = {5,5,5}, media = 0, i;
+//
+//        for (i = 0; i < vet.length; i++) {
+//            media += vet[i];
+//        }
+//        System.out.println(media);
+//        System.out.println(media/=i);
+
+
+//        int matriz[][] = {{10,20},{15,30}}, soma1 = 0, soma2 = 0, total = 0;
+//
+//        for (int i = 0; i < 2; i++) {
+//            soma1 += matriz[0][i];
+//            soma2 += matriz[1][i];
+//            for (int j = 0; j < 2; j++) {
+//                total += matriz[i][j];
+//            }
+//        }
+//        System.out.println("soma da primeira linha: "+ soma1);
+//        System.out.println("soma da segunda linha: "+ soma2);
+//        System.out.println("soma total: "+ total);
+//        System.out.println("diferenca das linhas: "+ (soma1 - soma2));
+
+        Scanner scanner = new Scanner(System.in);
+
+//        System.out.printf("Quantos valores vai informar: ");
+//        int indice = scanner.nextInt();
+//        scanner.nextLine();
+//
+//        if (indice > 0){
+//            int[] vet = new int[indice];
+//            int valor = 0;
+//
+//            for (int i = 0; i < vet.length; i++) {
+//                System.out.printf("Informe o valor %d: ", i);
+//                vet[i] = scanner.nextInt();
+//                scanner.nextLine();
+//                valor += vet[i];
+//            }
+//            System.out.println("\n Media: " + (valor /= indice));
+//        }
+
+//        String str = scanner.nextLine();
+//        while (!str.equalsIgnoreCase("fim")){
+//            System.out.println(str);
+//            str = scanner.nextLine();
+//        }
+    }
+}
