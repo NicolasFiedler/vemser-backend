9c4781b6f14a848fe1b8f4f0c3815ed9da44a346
Nicolas Fiedler <nicolas.fiedler@dbccompany.com.br>
2022-02-20 17:07:22 -0300

    feat: imprimir conta

--- aula-3/ContaBancaria/src/com/dbc/ContaCorrente.java
+++ aula-3/ContaBancaria/src/com/dbc/ContaCorrente.java
@@ -1,13 +1,20 @@
 package com.dbc;
 
 public class ContaCorrente {
-    public Cliente cliente;
+    public Cliente cliente = new Cliente();
     public String numeroConta;
     public Integer agencia;
     public Double saldo, chequeEspecial;
 
     public void imprimirContaCorrente(){
-
+        if (cliente != null){
+            cliente.imprimirCliente();
+        }
+        System.out.printf("Conta:   \t\t\t%s\n" +
+                "Agencia: \t\t\t%d\n" +
+                "Saldo:   \t\t\t%f\n" +
+                "Cheque Espercial:\t%f\n\n",
+                numeroConta, agencia, saldo, chequeEspecial);
     }
     public Boolean sacar(Double valor){
         return null;
