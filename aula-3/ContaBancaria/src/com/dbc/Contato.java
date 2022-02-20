15f8f2576a10d08ceb0dd23b3834e7aa78c78ab3
Nicolas Fiedler <nicolas.fiedler@dbccompany.com.br>
2022-02-20 12:25:37 -0300

    fix: crash null prints

--- aula-3/ContaBancaria/src/com/dbc/Contato.java
+++ aula-3/ContaBancaria/src/com/dbc/Contato.java
@@ -1,11 +1,13 @@
 package com.dbc;
 
 public class Contato {
-    public String descricao, telefone;
+    public String descricao = "", telefone;
     public Integer tipo;                        //Estava eu pensando num ternario pra deixar td em uma linha...
                                                 //Quando entao ouvi uma misterisa voz em minha cabeca,
     public void imprimirContato(){              //e ela diza: "nao faca isso, se colega vai querer te matar".
-        if (tipo == 1) {
+        if (tipo == null){
+            System.out.printf("Telefone: %s\nDescricao: %s\n\n", telefone, descricao);
+        }else if (tipo == 1) {
             System.out.printf("Telefone: %s\nContato: Residencial\nDescricao: %s\n\n", telefone, descricao);
         }else if (tipo == 2){
             System.out.printf("Telefone: %s\nContato: Comercial\nDescricao: %s\n\n", telefone, descricao);
