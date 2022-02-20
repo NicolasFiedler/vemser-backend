91e7bb4e1b6d07538a643b8df83128c36ea1bbc3
Nicolas Fiedler <nicolas.fiedler@dbccompany.com.br>
2022-02-20 12:22:43 -0300

    fix: espacamentos

--- aula-3/ContaBancaria/src/com/dbc/Endereco.java
+++ aula-3/ContaBancaria/src/com/dbc/Endereco.java
@@ -6,16 +6,18 @@ public class Endereco {
 
     public void imprimirEndereco(){                        //"Contato.java:5:49"
         if (!complemento.equals("")) {
-            System.out.printf("Endereco: %s, %d.\nComplemento: %s.\n\n", logradouro, numero, complemento);
+            System.out.printf("Endereco: \n%s, %d.\nComplemento: %s.\n", logradouro, numero, complemento);
             System.out.printf("%s, %s, %s, %s.\n", cidade, estado, cep, pais);
         } else {
-            System.out.printf("Endereco: %s, %d.\n\n", logradouro, numero);
+            System.out.printf("Endereco: %s, %d.\n", logradouro, numero);
             System.out.printf("%s, %s, %s, %s.\n", cidade, estado, cep, pais);
         }
-        if (tipo == 1){
-            System.out.println("Endereco residencial.\n");
+        if (tipo == null){
+        } else if (tipo == 1){
+            System.out.println("Endereco residencial.");
         } else if (tipo == 2) {
-            System.out.println("Endereco comercial\n");
+            System.out.println("Endereco comercial.");
         }
+        System.out.println("");
     }
 }
