7efb44f787e2d8f4821a25d7b01c0fbc62f9a2b3
Nicolas Fiedler <nicolas.fiedler@dbccompany.com.br>
2022-02-20 11:44:31 -0300

    feat: imprimi endereco

--- aula-3/ContaBancaria/src/com/dbc/Endereco.java
+++ aula-3/ContaBancaria/src/com/dbc/Endereco.java
@@ -2,9 +2,20 @@ package com.dbc;
 
 public class Endereco {
     public Integer tipo, numero;
-    public String logradouro, pais, estado, cidade, cep, complemento;
+    public String logradouro, pais, estado, cidade, cep, complemento = "";
 
-    public void imprimirEndereco(){
-
+    public void imprimirEndereco(){                        //"Contato.java:5:49"
+        if (!complemento.equals("")) {
+            System.out.printf("Endereco: %s, %d.\nComplemento: %s.\n\n", logradouro, numero, complemento);
+            System.out.printf("%s, %s, %s, %s.\n", cidade, estado, cep, pais);
+        } else {
+            System.out.printf("Endereco: %s, %d.\n\n", logradouro, numero);
+            System.out.printf("%s, %s, %s, %s.\n", cidade, estado, cep, pais);
+        }
+        if (tipo == 1){
+            System.out.println("Endereco residencial.");
+        } else if (tipo == 2) {
+            System.out.println("Endereco comercial");
+        }
     }
 }
