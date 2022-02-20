4db42de45dbc5fe53bf0f11ce1309359639b86da
Nicolas Fiedler <nicolas.fiedler@dbccompany.com.br>
2022-02-20 11:43:27 -0300

    feat: imprimir contato

--- aula-3/ContaBancaria/src/com/dbc/Contato.java
+++ aula-3/ContaBancaria/src/com/dbc/Contato.java
@@ -2,9 +2,13 @@ package com.dbc;
 
 public class Contato {
     public String descricao, telefone;
-    public Integer tipo;
-
-    public void imprimirContato(){
-
+    public Integer tipo;                        //Estava eu pensando num ternario pra deixar td em uma linha...
+                                                //Quando entao ouvi uma misterisa voz em minha cabeca,
+    public void imprimirContato(){              //e ela diza: "nao faca isso, se colega vai querer te matar".
+        if (tipo == 1) {
+            System.out.printf("Telefone: %s\nContato: Residencial\nDescricao: %s\n", telefone, descricao);
+        }else if (tipo == 2){
+            System.out.printf("Telefone: %s\nContato: Comercial\nDescricao: %s\n", telefone, descricao);
+        }
     }
 }
