0bc7d430838c8a7536c873fa0c19017de3968da6
Nicolas Fiedler <nicolas.fiedler@dbccompany.com.br>
2022-02-20 17:08:01 -0300

    testes de print

--- aula-3/ContaBancaria/src/com/dbc/Main.java
+++ aula-3/ContaBancaria/src/com/dbc/Main.java
@@ -1,8 +1,41 @@
 package com.dbc;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
+    private ContaCorrente cliente1, cliente2;
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-	// write your code here
+
+
+//        Cliente cliente = new Cliente();
+//
+//        Endereco endereco = new Endereco();
+//
+//       // cliente.enderecos[0] = endereco;
+//        cliente.imprimirContatos();
+//        cliente.imprimirCliente();
+//        cliente.imprimirEnderecos();
+
+        ContaCorrente contaCorrente = new ContaCorrente();
+        contaCorrente.imprimirContaCorrente();
+    }
+
+    public void start(){
+        //Gerando conta 1
+        cliente1 = new Cliente();
+
+        cliente1.
+    }
+
+    public void pressEnter(){
+        System.out.println("\n Pressione [ENTER] para continuar.");
+        scanner.nextLine();
+        for (int i = 0; i < 50; i++) {
+            System.out.println("");
+        }
     }
 }
