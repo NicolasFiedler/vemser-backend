1412d2777afb4e0fc599da18681c7c19503455ed
Nicolas Fiedler <nicolas.fiedler@dbccompany.com.br>
2022-02-20 12:22:07 -0300

    fix: print vetores

--- aula-3/ContaBancaria/src/com/dbc/Cliente.java
+++ aula-3/ContaBancaria/src/com/dbc/Cliente.java
@@ -6,13 +6,33 @@ public class Cliente {
     public Endereco[] enderecos = new Endereco[2];
 
     public void imprimirCliente(){
-
+        System.out.printf("Nome: %s \nCpf: %s\n\n", nome, cpf);
     }
     public void imprimirContatos(){
-
+        Boolean flag = true;
+        for (Contato contato : contatos) {
+            if (contato == null){
+                if (flag){
+                    System.out.println("Nao ha contatos cadastrados");
+                }
+                break;
+            }
+            flag = false;
+            contato.imprimirContato();
+        }
     }
     public void imprimirEnderecos(){
-
+        Boolean flag = true;
+        for (Endereco endereco : enderecos) {
+            if (endereco == null){
+                if (flag){
+                    System.out.println("Nao ha enderecos cadastrados");
+                }
+                break;
+            }
+            flag = false;
+            endereco.imprimirEndereco();
+        }
     }
 }
 
