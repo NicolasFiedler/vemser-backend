684816d89387734d4e7acf1aef5954ecba08f038
Nicolas Fiedler <nicolas.fiedler@dbccompany.com.br>
2022-02-21 01:31:48 -0300

    feat: transacoes bancarias

--- aula-3/ContaBancaria/src/com/dbc/ContaCorrente.java
+++ aula-3/ContaBancaria/src/com/dbc/ContaCorrente.java
@@ -1,31 +1,86 @@
 package com.dbc;
 
 public class ContaCorrente {
-    public Cliente cliente = new Cliente();
+    public ContaCorrente(){
+        cliente = new Cliente();
+    }
+    public ContaCorrente(String nome, String cpf, String numeroConta, Double saldo){
+        cliente = new Cliente(nome, cpf);
+        this.numeroConta = numeroConta;
+        this.saldo = saldo;
+    }
+
+
+    public Cliente cliente;
     public String numeroConta;
-    public Integer agencia;
-    public Double saldo, chequeEspecial;
+    public Integer agencia = 1111;
+    public Double saldo, chequeEspecial = 200., valorChequeEspecial = chequeEspecial;
+
+
 
     public void imprimirContaCorrente(){
+        System.out.printf("Conta:   \t\t\t%s\n" +
+                          "Agencia: \t\t\t%d\n" +
+                          "Saldo:   \t\t\tR$ %.2f\n" +
+                          "Cheque Espercial:\tR$ %.2f\n\n",
+                numeroConta, agencia, saldo, chequeEspecial);
         if (cliente != null){
             cliente.imprimirCliente();
         }
-        System.out.printf("Conta:   \t\t\t%s\n" +
-                "Agencia: \t\t\t%d\n" +
-                "Saldo:   \t\t\t%f\n" +
-                "Cheque Espercial:\t%f\n\n",
-                numeroConta, agencia, saldo, chequeEspecial);
     }
+
     public Boolean sacar(Double valor){
-        return null;
+        if (valor > 0 && retornarSaldoComChequeEspecial() >= valor && saldo > 0){
+            if (valor > saldo){
+                Double valorTotal = valor;
+                valor -= saldo;
+                saldo -= valorTotal;
+                chequeEspecial -= valor;
+                return true;
+            } else {
+                saldo -= valor;
+                return true;
+            }
+        } else if (saldo < 0 && chequeEspecial > 0 && valor <= chequeEspecial){
+            saldo -= valor;
+            chequeEspecial -= valor;
+            return true;
+        }
+        return false;
     }
+
     public Boolean depositar(Double valor){
-        return null;
+        if (valor > 0){
+            if (saldo < 0){
+                if (valor <= valorChequeEspecial - chequeEspecial){
+                    chequeEspecial += valor;
+                    saldo += valor;
+                } else {
+                    saldo = valor - (valorChequeEspecial - chequeEspecial);
+                    chequeEspecial = valorChequeEspecial;
+                }
+            }else {
+                saldo += valor;
+                return true;
+            }
+        }
+        return false;
     }
+
     public Double retornarSaldoComChequeEspecial(){
-        return null;
+        if (saldo > 0){
+            return saldo + chequeEspecial;
+        } else {
+            return chequeEspecial;
+        }
     }
-    public Boolean transferir(ContaCorrente contaCorrente, Double valor){
-        return null;
+
+    public Boolean transferir(ContaCorrente destinatario, Double valor){
+        if ((!this.numeroConta.equals(destinatario.numeroConta)) && this.sacar(valor)){
+            if (destinatario.depositar(valor)){
+                return true;
+            }
+        }
+        return false;
     }
 }
