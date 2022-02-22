017c40af8f8589c1f076ae86a6ef30195a392b77
Nicolas Fiedler <nicolas.fiedler@dbccompany.com.br>
2022-02-22 09:29:45 -0300

    fix: bateria de testes

--- aula-4/ContaBancaria/src/com/dbc/Main.java
+++ aula-4/ContaBancaria/src/com/dbc/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     private static ContaCorrente conta1, conta2;
+    private static ContaPoupanca conP1;
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -13,61 +14,79 @@ public class Main {
 
         start();
 
-        conta1.imprimirContaCorrente();
+        conta1.imprimir();
         pressEnter();
 
         //DEMONSTRACAO DE SAQUES
 
         System.out.println("SAQUE DE R$ 100,00\n");
         conta1.sacar(100.);
-        conta1.imprimirContaCorrente();
+        conta1.imprimir();
         pressEnter();
 
         System.out.println("SAQUE DE R$ 500,00\n");
         conta1.sacar(500.);
-        conta1.imprimirContaCorrente();
+        conta1.imprimir();
         pressEnter();
 
         System.out.println("SAQUE DE R$ 10,00\n");
         conta1.sacar(10.);
-        conta1.imprimirContaCorrente();
+        conta1.imprimir();
         pressEnter();
 
         System.out.println("(tentativa) SAQUE DE R$ 100,00\n");
         if (!conta1.sacar(100.)){
             System.out.println("Saldo insuficiente.");
         }
-        conta1.imprimirContaCorrente();
+        conta1.imprimir();
         pressEnter();
 
         //DEMONSTRACAO DE DEPOSITOS
 
         System.out.println("DEPOSITO DE R$ 90,00\n");
         conta1.depositar(90.);
-        conta1.imprimirContaCorrente();
+        conta1.imprimir();
         pressEnter();
 
         System.out.println("DEPOSITO DE R$ 230,00\n");
         conta1.depositar(230.);
-        conta1.imprimirContaCorrente();
+        conta1.imprimir();
         pressEnter();
 
         //DEMONSTRACAO DE TRANSFERENCIA
 
         System.out.println("OBSERVER O ESTADO ATUAL DA CONTA #1\n");
-        conta1.imprimirContaCorrente();
+        conta1.imprimir();
         pressEnter();
         System.out.println("OBSERVER O ESTADO ATUAL DA CONTA #2\n");
-        conta2.imprimirContaCorrente();
+        conta2.imprimir();
         pressEnter();
 
         System.out.println("TRANSFERENCIAS DE R$ 130,00 DA CONTA #1 PARA #2\n");
         conta1.transferir(conta2,130.);
-        conta1.imprimirContaCorrente();
+        conta1.imprimir();
         pressEnter();
 
         System.out.println("TRANSFERENCIAS DE R$ 130,00 DA CONTA #1 PARA #2\n");
-        conta2.imprimirContaCorrente();
+        conta2.imprimir();
+        pressEnter();
+
+        //TRANSFERENCIA CONTA POUPANCA PARA CONRRENTE
+
+        System.out.println("OBSERVER O ESTADO ATUAL DA CONTA #1\n");
+        conP1.imprimir();
+        pressEnter();
+        System.out.println("OBSERVER O ESTADO ATUAL DA CONTA #2\n");
+        conta2.imprimir();
+        pressEnter();
+
+        System.out.println("TRANSFERENCIAS DE R$ 100,00 DA CONTA #1 PARA #2\n");
+        conP1.transferir(conta2,100.);
+        conP1.imprimir();
+        pressEnter();
+
+        System.out.println("TRANSFERENCIAS DE R$ 100,00 DA CONTA #1 PARA #2\n");
+        conta2.imprimir();
     }
 
     public static void start(){
@@ -75,7 +94,7 @@ public class Main {
         conta1 = new ContaCorrente("Mateus",
                                    "123.456.789-00",
                                    "12345678-9",
-                                   500.);
+                                   "0001", 500., 200.);
         Contato[] contatos = new Contato[2];
 
         contatos[0] = new Contato("91234-1234",
@@ -89,7 +108,7 @@ public class Main {
         conta2 = new ContaCorrente("Jorge",
                 "987.654.321-00",
                 "98765432-1",
-                500.);
+                "0001", 500., 200.);
         contatos[0] = new Contato("94321-4321",
                 2,
                 "Atendemos das 8:00 as 17:00");
@@ -98,6 +117,20 @@ public class Main {
                 "");
         conta2.getCliente().setContatos(contatos);
         conta2.getCliente().setEnderecos(new Endereco[2]);
+
+        //Gerando conta poupanca
+        conP1 = new ContaPoupanca("Lucas",
+                "xxx.xxx.xxx-xx",
+                "555555-1",
+                "0001", 500.);
+        contatos[0] = new Contato("94321-4321",
+                2,
+                "aquele do sobrenome dificil");
+        contatos[1] = new Contato("94321-1234",
+                1,
+                "");
+        conP1.getCliente().setContatos(contatos);
+        conP1.getCliente().setEnderecos(new Endereco[2]);
     }
 
     public static void pressEnter(){
