6e1c59a1918d84c657f2f475c3ca1e58d0092958
Nicolas Fiedler <nicolas.fiedler@dbccompany.com.br>
2022-02-21 23:45:46 -0300

    feat: getters & setters

--- aula-4/ContaBancaria/src/com/dbc/Endereco.java
+++ aula-4/ContaBancaria/src/com/dbc/Endereco.java
@@ -26,7 +26,69 @@ public class Endereco {
     private Integer tipo, numero;
     private String logradouro, pais, estado, cidade, cep, complemento = "";
 
+    public Integer getTipo() {
+        return tipo;
+    }
 
+    public void setTipo(Integer tipo) {
+        this.tipo = tipo;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
 
     public void imprimirEndereco(){                        //"Contato.java:16:49"
         if (!complemento.equals("")) {
