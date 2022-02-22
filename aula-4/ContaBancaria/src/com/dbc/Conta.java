8d8148bc100d2da803cb5fe71470e0d3d7e43205
Nicolas Fiedler <nicolas.fiedler@dbccompany.com.br>
2022-02-21 23:39:54 -0300

    feat: conta abstrata

---
+++ aula-4/ContaBancaria/src/com/dbc/Conta.java
@@ -0,0 +1,68 @@
+package com.dbc;
+
+import java.util.logging.SocketHandler;
+
+public abstract class Conta implements Movimentacao {
+
+    private Cliente cliente;
+    private String numeroConta, agencia;
+    private Double saldo;
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(String numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public Boolean sacar (Double valor){
+        if (valor < saldo){
+            saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public Boolean depositar(Double valor){
+        if (valor > 0){
+            saldo += valor;
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public Boolean transferir(Conta conta, Double valor){
+        if ((!this.numeroConta.equals(conta.numeroConta)) && this.sacar(valor)){
+            if (conta.depositar(valor)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
