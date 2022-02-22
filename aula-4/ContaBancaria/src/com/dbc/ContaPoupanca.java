36f1d54f3433f8db10ec9cfa34d9890555323d9f
Nicolas Fiedler <nicolas.fiedler@dbccompany.com.br>
2022-02-22 09:29:09 -0300

    feat: imprimir / feat: construtores

--- aula-4/ContaBancaria/src/com/dbc/ContaPoupanca.java
+++ aula-4/ContaBancaria/src/com/dbc/ContaPoupanca.java
@@ -4,12 +4,23 @@ public class ContaPoupanca extends Conta implements Impressao{
 
     private static final Double JUROS_MENSAL = 1.01;
 
+    public ContaPoupanca(String nome, String cpf, String numeroConta, String agencia, Double saldo) {
+        super(nome, cpf, numeroConta, agencia, saldo);
+        creditarTaxa();
+    }
+
     public void creditarTaxa () {
         setSaldo(getSaldo()*JUROS_MENSAL);
     }
 
     @Override
     public void imprimir() {
-        //todo montar o imprimir da poupanca;
+        System.out.printf("Conta:   \t\t\t%s\n" +
+                        "Agencia: \t\t\t%s\n" +
+                        "Saldo:   \t\t\tR$ %.2f\n\n",
+                getNumeroConta(), getAgencia(), getSaldo());
+        if (getCliente() != null){
+            getCliente().imprimirCliente();
+        }
     }
 }
